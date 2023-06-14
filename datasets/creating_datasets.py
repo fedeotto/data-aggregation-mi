1f2b87db768d0cb732d615a56ef557fd2ab93b32
FedeOtto <federicoottomano94@gmail.com>
2023-06-14 23:55:30 +0100

    datasets check

--- datasets/creating_datasets.py
+++ datasets/creating_datasets.py
@@ -38,11 +38,11 @@ mpds_bulk_modulus.to_csv('mpds_shear_modulus.csv',index=False)
 
 # %% AFLOW DATA
 
-aflow_database = pd.read_csv('master_icsd.csv')
+aflow_database = pd.read_csv('./datasets/master_icsd.csv')
 
 # aflow bulk modulus
 aflow_bulk_modulus = aflow_database[['formula','ael_bulk_modulus_vrh']].dropna()
-aflow_bulk_modulus = aflow_bulk_modulus.drop_duplicates('formula',keep='last').reset_index(drop=True)
+# aflow_bulk_modulus = aflow_bulk_modulus.drop_duplicates('formula',keep='last').reset_index(drop=True)
 aflow_bulk_modulus = aflow_bulk_modulus.rename(columns={'ael_bulk_modulus_vrh' : 'target'})
 
 # aflow_shear_modulus
